7ea8e0b148e9d1c77f083fe493e1a5c252ef0ece
AL-SAFWAN <al-safwan.uddin@abehr.com>
2023-12-04 22:21:26 +0000

    following player based on impuse

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import {
   Physics,
   RigidBody,
   useRapier,
+  vec3,
 } from "@react-three/rapier";
 import * as THREE from "three";
 import * as RAPIER from "@dimforge/rapier3d-compat";
@@ -30,24 +31,25 @@ function Box(props) {
   const [hovered, hover] = useState(false);
   const [clicked, click] = useState(false);
   // Subscribe this component to the render-loop, rotate the mesh every frame
+
   useFrame((state, delta) => (ref.current.rotation.x += delta * count.current));
   // Return the view, these are regular Threejs elements expressed in JSX
   return (
-    <RigidBody>
+    <RigidBody type="dynamic">
       <mesh
         {...props}
         ref={ref}
         scale={clicked ? 1.5 : 1}
-        onClick={(event) => {
-          console.log(count);
-          count.current += 1;
-          if (count.current == 10) {
-            count.current = 1;
-          }
-          click(!clicked);
-        }}
-        onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-        onPointerOut={(event) => hover(false)}
+        // onClick={(event) => {
+        //   console.log(count);
+        //   count.current += 1;
+        //   if (count.current == 10) {
+        //     count.current = 1;
+        //   }
+        //   click(!clicked);
+        // }}
+        // onPointerOver={(event) => (event.stopPropagation(), hover(true))}
+        // onPointerOut={(event) => hover(false)}
       >
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
@@ -80,53 +82,62 @@ const rotation = new THREE.Vector3();
 
 export function Player() {
   const ref = useRef();
+  const camRef = useRef();
   const rapier = useRapier();
   const [, get] = useKeyboardControls();
 
   useFrame((state) => {
     const { forward, backward, left, right, jump, shift } = get();
+    if (camRef.current) {
+      const cameraDistanceY = window.innerWidth < 1024 ? 16 : 20;
+      const cameraDistanceZ = window.innerWidth < 1024 ? 12 : 16;
+      const playersWorldPos = vec3(ref.current.translation());
 
-    const velocity = ref.current.linvel();
-
-    let p = ref.current.translation();
-    // state.camera.position.set(p.x, p.y, p.z);
-
-    // // movement
-    frontVector.set(0, 0, backward - forward);
-    sideVector.set(left - right, 0, 0);
-
-    direction
-      .subVectors(frontVector, sideVector)
-      .normalize()
-      .multiplyScalar(shift ? 10 : 5)
-      .applyEuler(state.camera.rotation);
-    // console.log(direction, velocity, ref.current);
-
-    ref.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z });
-
-    // // // jumping
-    // const world = rapier.world.raw();
-    // const ray = world.castRay(
-    //   new RAPIER.Ray(ref.current.translation(), { x: 0, y: -1, z: 0 })
-    // );
-    // const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1.75;
-    // if (jump && grounded) ref.current.setLinvel({ x: 0, y: 7.5, z: 0 });
+      camRef.current.setLookAt(
+        playersWorldPos.x,
+        playersWorldPos.y + cameraDistanceY,
+        playersWorldPos.z + cameraDistanceZ + 15,
+        playersWorldPos.x,
+        playersWorldPos.y + 1.5,
+        playersWorldPos.z - 2,
+        true
+      );
+    }
+    let speed = shift ? 5 : 1;
+    if (right) {
+      ref.current.applyImpulse({ x: speed, y: 0, z: 0 }, true);
+    }
+    if (left) {
+      ref.current.applyImpulse({ x: -speed, y: 0, z: 0 }, true);
+    }
+    if (forward) {
+      ref.current.applyImpulse({ x: 0, y: 0, z: -speed }, true);
+    }
+    if (backward) {
+      ref.current.applyImpulse({ x: 0, y: 0, z: speed }, true);
+    }
+    console.log(jump);
+    if (jump) {
+      ref.current.applyImpulse({ x: 0, y: 1, z: 0 }, true);
+    }
   });
   return (
     <>
+      {" "}
+      <CameraControls ref={camRef} />
       <RigidBody
         ref={ref}
-        colliders={false}
+        colliders={"cuboid"}
         mass={1}
         type="dynamic"
-        position={[0, 50, 0]}
-        enabledRotations={[false, false, false]}
+        position={[0, 0, 0]}
+        lockRotations
+        linearDamping={14}
       >
-        <Sphere args={[1]}>
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
           <meshStandardMaterial color={"hotpink"} />
-        </Sphere>
-
-        <BallCollider args={[0.75, 0.5]} />
+        </mesh>
       </RigidBody>
     </>
   );
@@ -146,7 +157,7 @@ function App() {
   //     { name: Controls.back, keys: ["ArrowDown", "KeyS"] },
   //     { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
   //     { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
-  //     { name: Controls.jump, keys: ["Space"] },
+  //
   //   ],
   //   []
   // );
@@ -157,18 +168,19 @@ function App() {
         { name: "backward", keys: ["ArrowDown", "s", "S"] },
         { name: "left", keys: ["ArrowLeft", "a", "A"] },
         { name: "right", keys: ["ArrowRight", "d", "D"] },
+        { name: "jump", keys: ["Space"] },
         { name: "shift", keys: ["Shift"] },
       ]}
     >
-      <Canvas shadows camera={{ fov: 80, position: [5, 5, 5] }}>
+      <Canvas shadows camera={{ fov: 20 }}>
         <ambientLight intensity={Math.PI / 2} />
-        <spotLight
+        {/* <spotLight
           position={[10, 10, 10]}
           angle={0.15}
           penumbra={1}
           decay={0}
           intensity={Math.PI}
-        />
+        /> */}
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         <Physics gravity={[0, -30, 0]} debug>
           <Sky sunPosition={[100, 20, 100]} />
@@ -177,8 +189,6 @@ function App() {
           <Box position={[1, 1, 0]} />
           <Player />
         </Physics>
-        {/* <OrbitControls /> */}
-
         <PointerLockControls />
       </Canvas>
     </KeyboardControls>
