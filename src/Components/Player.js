6943362a0221a40cfee97cd50309258ffcec3414
AL-SAFWAN <al-safwan.uddin@abehr.com>
2023-12-08 09:34:42 +0000

    refactor move player and camera

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -3,50 +3,54 @@ import { useKeyboardControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { CapsuleCollider, RigidBody, quat, vec3 } from "@react-three/rapier";
 import { Vector3 } from "three";
+
+const moveCamera = (player, camera, changeView) => {
+  let playerPos = vec3(player.translation());
+  let playerQuat = quat(player.rotation());
+
+  let playerOffset = !changeView ? new Vector3(0, 1, 5) : new Vector3(0, 0, 0);
+  let alpha = changeView ? 0.05 : 0.27;
+
+  camera.position.lerp(
+    playerOffset.applyQuaternion(playerQuat).add(playerPos),
+    alpha
+  );
+};
+
+const movePlayer = (player, camera, controls) => {
+  let { forward, backward, left, right, jump, shift } = controls;
+  player.setRotation(camera.quaternion);
+
+  const forwardVector = camera.getWorldDirection(new Vector3());
+  const vectorUp = new Vector3(0, 1, 0);
+  const sideVector = vectorUp.cross(forwardVector);
+
+  let speed = shift ? 5 : 1;
+  // based on camera world direction
+  const directionalVector = forwardVector
+    .multiplyScalar(-(backward - forward) * speed)
+    .add(sideVector.multiplyScalar(-(right - left) * speed));
+
+  player.applyImpulse(
+    {
+      x: directionalVector.x,
+      y: jump ? 1 : 0,
+      z: directionalVector.z,
+    },
+    true
+  );
+};
+
 const Player = () => {
   const player = useRef();
   const [, get] = useKeyboardControls();
 
-  const moveCamera = (camera, q) => {
-    let playerPos = vec3(player.current.translation());
-    let playerQuat = quat(player.current.rotation());
-
-    let playerOffset = !q ? new Vector3(0, 1, 5) : new Vector3(0, 0, 0);
-
-    let alpha = q ? 0.05 : 0.27;
-
-    camera.position.lerp(
-      playerOffset.applyQuaternion(playerQuat).add(playerPos),
-      alpha
-    );
-  };
-  const movePlayer = (player, camera, controls) => {
-    let { forward, backward, left, right, jump, shift } = controls;
-    player.setRotation(camera.quaternion);
-
-    const forwardVector = camera.getWorldDirection(new Vector3());
-    const vectorUp = new Vector3(0, 1, 0);
-    const sideVector = vectorUp.cross(forwardVector);
-
-    let speed = shift ? 5 : 1;
-    // based on camera world direction
-    const directionalVector = forwardVector
-      .multiplyScalar(-(backward - forward) * speed)
-      .add(sideVector.multiplyScalar(-(right - left) * speed));
-
-    player.applyImpulse(
-      {
-        x: directionalVector.x,
-        y: jump ? 1 : 0,
-        z: directionalVector.z,
-      },
-      true
-    );
-  };
-  useFrame((state, delta) => {
+  useFrame((state) => {
     const controls = get();
-    moveCamera(state.camera, controls.q);
-    movePlayer(player.current, state.camera, controls);
+    if (player?.current) {
+      moveCamera(player.current, state.camera, controls.q);
+      movePlayer(player.current, state.camera, controls);
+    }
   });
 
   return (
