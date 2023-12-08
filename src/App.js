1acccf6d696c9f996493838c620c5f1df46639e9
AL-SAFWAN <al-safwan.uddin@abehr.com>
2023-12-08 09:24:17 +0000

    cleaning up player

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import {
-  CameraControls,
-  KeyboardControls,
-  OrbitControls,
-  PointerLockControls,
-  Sky,
-  Sphere,
-  useKeyboardControls,
-  useTexture,
-} from "@react-three/drei";
+import { KeyboardControls, PointerLockControls, Sky } from "@react-three/drei";
 import "./App.css";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useMemo, useRef, useState } from "react";
-import {
-  BallCollider,
-  CapsuleCollider,
-  CuboidCollider,
-  Physics,
-  RapierRigidBody,
-  RigidBody,
-  quat,
-  useRapier,
-  vec3,
-} from "@react-three/rapier";
-import * as THREE from "three";
-import * as RAPIER from "@dimforge/rapier3d-compat";
-import { radToDeg } from "three/src/math/MathUtils";
+import { CuboidCollider, Physics, RigidBody } from "@react-three/rapier";
+import Player from "./Components/Player";
 
+function Room(props) {
+  const ref = useRef();
+
+  return (
+    <>
+      <RigidBody type="fixed" colliders={"cuboid"}>
+        <mesh {...props} ref={ref} position={[-30, 1.5, 0]}>
+          <boxGeometry args={[30, 3, 1]} />
+          <meshStandardMaterial color={"black"} />
+        </mesh>
+      </RigidBody>
+
+      <RigidBody type="fixed" colliders={"cuboid"}>
+        <mesh {...props} ref={ref} position={[0, 1.5, -30]}>
+          <boxGeometry args={[30, 3, 1]} />
+          <meshStandardMaterial color={"black"} />
+        </mesh>
+      </RigidBody>
+    </>
+  );
+}
 function Box(props) {
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef();
@@ -61,60 +61,6 @@ function Box(props) {
   );
 }
 
-// let offset = new THREE.Vector3(0, 1, 3)
-//   .applyQuaternion(quat(ref.current.rotation()))
-//   .add(vec3(ref.current.translation()));
-
-// let lookAt = new THREE.Vector3(0, 0.5, 1)
-//   .applyQuaternion(quat(ref.current.rotation()))
-//   .add(vec3(ref.current.translation()));
-
-// state.camera.position.copy(offset);
-// boxRef.current.position(offset);
-// refControl.current.onMouseMove((e) => {
-//   console.log("hello");
-// });
-
-// useFrame((state, delta) => {
-//   const { forward, backward, left, right, jump, shift } = get();
-
-//   // if (boxRef.current) {
-//   const cameraDistanceY = window.innerWidth < 1024 ? 16 : 20;
-//   const cameraDistanceZ = window.innerWidth < 1024 ? 12 : 16;
-//   const playersWorldPos = vec3(ref.current.translation());
-
-//   let radius = 3;
-//   x = playersWorldPos.x + radius * Math.sin(state.camera.rotation.y);
-//   z = playersWorldPos.z + radius * Math.cos(state.camera.rotation.y);
-//   ref.current.setRotation(state.camera.quaternion);
-//   // ref.current.setRotation(state.camera.rotation.y);
-//   // boxRef.current.setLookAt(
-//   //   x,
-//   //   1.5,
-//   //   z,
-
-//   //   playersWorldPos.x,
-//   //   playersWorldPos.y,
-//   //   playersWorldPos.z
-//   //   // true
-//   // );
-
-//   state.camera.position.copy({ x, y: 0, z }).add({ x: 0, y: 3, z: 0 });
-//   // state.camera.lookAt(playersWorldPos);
-//   // }
-//   // movement
-
-// let speed = shift ? 5 : 1;
-//   ref.current.applyImpulse(
-//     {
-//       x: (right - left) * speed,
-//       y: jump ? 1 : 0,
-//       z: (backward - forward) * speed,
-//     },
-//     true
-//   );
-// });
-
 function Ground(props) {
   return (
     <RigidBody {...props} type="fixed" colliders={false}>
@@ -130,77 +76,6 @@ function Ground(props) {
     </RigidBody>
   );
 }
-
-const SPEED = 5;
-const direction = new THREE.Vector3();
-const frontVector = new THREE.Vector3();
-const sideVector = new THREE.Vector3();
-const rotation = new THREE.Vector3();
-
-export function Player() {
-  const player = useRef();
-  const [, get] = useKeyboardControls();
-
-  let moveCamera = (q, state) => {};
-
-  useFrame((state, delta) => {
-    const { forward, backward, left, right, jump, shift, q } = get();
-    let playerPos = vec3(player.current.translation());
-    let playerQuat = quat(player.current.rotation());
-
-    let playerOffset = q
-      ? new THREE.Vector3(0, 1, 5)
-      : new THREE.Vector3(0, 0, 0);
-    let alpha = q ? 0.05 : 0.27;
-
-    state.camera.position.lerp(
-      playerOffset.applyQuaternion(playerQuat).add(playerPos),
-      alpha
-    );
-
-    // rotation need to be set before movement
-    player.current.setRotation(state.camera.quaternion);
-
-    const forwardVector = state.camera.getWorldDirection(new THREE.Vector3());
-    const vectorUp = new THREE.Vector3(0, 1, 0);
-    const sideVector = vectorUp.cross(forwardVector);
-
-    let speed = shift ? 5 : 1; // based on camera direction
-    const directionalVector = forwardVector
-      .multiplyScalar(-(backward - forward) * speed)
-      .add(sideVector.multiplyScalar(-(right - left) * speed));
-
-    player.current.applyImpulse(
-      {
-        x: directionalVector.x,
-        y: jump ? 1 : 0,
-        z: directionalVector.z,
-      },
-      true
-    );
-  });
-
-  return (
-    <>
-      <RigidBody
-        ref={player}
-        colliders={false}
-        mass={1}
-        type="dynamic"
-        lockRotations
-        position={[0, 0, 0]}
-        linearDamping={14}
-      >
-        <mesh>
-          <capsuleGeometry args={[0.5, 1.5]} />
-          <meshStandardMaterial color={"lime"} />
-        </mesh>
-        <CapsuleCollider args={[0.75, 0.5]} />
-      </RigidBody>
-    </>
-  );
-}
-
 function App() {
   let pc = useRef();
   return (
@@ -225,10 +100,11 @@ function App() {
           intensity={Math.PI}
         />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Physics gravity={[0, -30, 0]}>
+        <Physics gravity={[0, -30, 0]} debug>
           <Sky sunPosition={[100, 20, 100]} />
           <Ground />
           <Player pc={pc} />
+          <Room />
           <Box position={[1, 3, 1]} />
         </Physics>
         <PointerLockControls ref={pc} />
---
+++ src/Components/Player.js
@@ -0,0 +1,73 @@
+import React, { useRef } from "react";
+import { useKeyboardControls } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { CapsuleCollider, RigidBody, quat, vec3 } from "@react-three/rapier";
+import { Vector3 } from "three";
+const Player = () => {
+  const player = useRef();
+  const [, get] = useKeyboardControls();
+
+  const moveCamera = (camera, q) => {
+    let playerPos = vec3(player.current.translation());
+    let playerQuat = quat(player.current.rotation());
+
+    let playerOffset = !q ? new Vector3(0, 1, 5) : new Vector3(0, 0, 0);
+
+    let alpha = q ? 0.05 : 0.27;
+
+    camera.position.lerp(
+      playerOffset.applyQuaternion(playerQuat).add(playerPos),
+      alpha
+    );
+  };
+  const movePlayer = (player, camera, controls) => {
+    let { forward, backward, left, right, jump, shift } = controls;
+    player.setRotation(camera.quaternion);
+
+    const forwardVector = camera.getWorldDirection(new Vector3());
+    const vectorUp = new Vector3(0, 1, 0);
+    const sideVector = vectorUp.cross(forwardVector);
+
+    let speed = shift ? 5 : 1;
+    // based on camera world direction
+    const directionalVector = forwardVector
+      .multiplyScalar(-(backward - forward) * speed)
+      .add(sideVector.multiplyScalar(-(right - left) * speed));
+
+    player.applyImpulse(
+      {
+        x: directionalVector.x,
+        y: jump ? 1 : 0,
+        z: directionalVector.z,
+      },
+      true
+    );
+  };
+  useFrame((state, delta) => {
+    const controls = get();
+    moveCamera(state.camera, controls.q);
+    movePlayer(player.current, state.camera, controls);
+  });
+
+  return (
+    <>
+      <RigidBody
+        ref={player}
+        colliders={false}
+        mass={1}
+        type="dynamic"
+        lockRotations
+        position={[0, 0, 0]}
+        linearDamping={14}
+      >
+        <mesh>
+          <capsuleGeometry args={[0.5, 1.5]} />
+          <meshStandardMaterial color={"lime"} />
+        </mesh>
+        <CapsuleCollider args={[0.75, 0.5]} />
+      </RigidBody>
+    </>
+  );
+};
+
+export default Player;
