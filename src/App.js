f926eceabb1a2241fa481b5f4e79ae37d17e649d
AL-SAFWAN <al-safwan.uddin@abehr.com>
2023-12-11 22:03:14 +0000

    refactored coin

--- src/App.js
+++ src/App.js
@@ -1,58 +1,11 @@
 import { KeyboardControls, PointerLockControls, Sky } from "@react-three/drei";
 import "./App.css";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Suspense, useEffect, useMemo, useRef, useState } from "react";
-import { CuboidCollider, Physics, RigidBody, vec3 } from "@react-three/rapier";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useEffect, useState } from "react";
+import { CuboidCollider, Physics, RigidBody } from "@react-three/rapier";
 import Player from "./Components/Player";
+import Coin from "./Components/Coin";
 
-function Coin({ x = 5, z = 5, removeCoin, id }) {
-  const ref = useRef();
-  console.log("coin");
-  useFrame((_, delta) => {
-    if (ref.current) {
-      ref.current.setTranslation({
-        x: x,
-        y: Math.sin(Date.now() * 0.002) * 0.2 + 1.4,
-        z: z,
-      });
-    }
-  });
-
-  return (
-    <>
-      <RigidBody
-        name="coin"
-        ref={ref}
-        type="fixed"
-        colliders={"trimesh"}
-        onCollisionEnter={({ manifold, target, other }) => {
-          if (other.rigidBodyObject) {
-            removeCoin(id);
-            console.log(id);
-            console.log(
-              // this rigid body's Object3D
-              target.rigidBodyObject.name,
-              " collided with ",
-              // the other rigid body's Object3D
-              other.rigidBodyObject.name
-            );
-          }
-        }}
-        mass={1}
-      >
-        <mesh rotation={[Math.PI / 2, 0, 0]} position={[x, 0, z]}>
-          <pointLight intensity={1} distance={5} decay={1.5} color={"gold"} />
-          <cylinderGeometry args={[0.2, 0.2, 0.1]} />
-          <meshStandardMaterial
-            color={"gold"}
-            roughness={0.4}
-            metalness={0.1}
-          />
-        </mesh>
-      </RigidBody>
-    </>
-  );
-}
 function Box() {
   return (
     <RigidBody type="dynamic" position={[5, 2, 10]}>
@@ -70,38 +23,29 @@ function Ground(props) {
     <RigidBody {...props} type="fixed" colliders={false} friction={2}>
       <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
         <planeGeometry args={[1000, 1000]} />
-        <meshStandardMaterial
-          // map={texture}
-          // map-repeat={[240, 240]}
-          color="springgreen"
-        />
+        <meshStandardMaterial color="springgreen" />
       </mesh>
       <CuboidCollider args={[1000, 2, 1000]} position={[0, -2, 0]} />
     </RigidBody>
   );
 }
-function App() {
-  console.log("app is render");
-  const [coins, setCoin] = useState([
-      <Coin removeCoin={removeCoin} x={1} z={1} key={0} id={0} />,
-      <Coin removeCoin={removeCoin} x={1} z={2} key={1} id={1} />,
-      <Coin removeCoin={removeCoin} x={1} z={3} key={2} id={2} />,
-      <Coin removeCoin={removeCoin} x={1} z={-1} key={3} id={3} />,
-      <Coin removeCoin={removeCoin} x={1} z={-2} key={4} id={4} />,
-      <Coin removeCoin={removeCoin} x={1} z={-3} key={5} id={5} />,
-    ]);
 
-  let removeCoin = (id) => {
-    console.log(coins);
-    let filterArr = coins.filter((coin) => coin.props.id != id);
-    console.log(filterArr, coins);
-    // setCoin(filterArr);
-  };
-
+function Coins() {
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    setCoin();
+    setCoins([
+      <Coin setCoins={setCoins} x={1} z={1} key={0} id={0} />,
+      <Coin setCoins={setCoins} x={1} z={2} key={1} id={1} />,
+      <Coin setCoins={setCoins} x={1} z={3} key={2} id={2} />,
+      <Coin setCoins={setCoins} x={1} z={-1} key={3} id={3} />,
+      <Coin setCoins={setCoins} x={1} z={-2} key={4} id={4} />,
+      <Coin setCoins={setCoins} x={1} z={-3} key={5} id={5} />,
+    ]);
   }, []);
+  return coins.map((coin) => coin);
+}
 
+function App() {
   return (
     <KeyboardControls
       map={[
@@ -125,7 +69,7 @@ function App() {
           <Physics gravity={[0, -9.81, 0]} debug>
             <Sky sunPosition={[100, 20, 100]} />
             <Ground />
-            {coins.map((e) => e)}
+            <Coins />
             <RigidBody type="fixed" colliders={"cuboid"}>
               <mesh
                 receiveShadow
@@ -147,7 +91,6 @@ function App() {
               </mesh>
             </RigidBody>
             <Player />
-            {/* <Coin /> */}
             <Box position={[1, 3, 1]} />
           </Physics>
           <PointerLockControls />
---
+++ src/Components/Coin.js
@@ -0,0 +1,51 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { RigidBody } from "@react-three/rapier";
+
+const Coin = ({ x = 5, z = 5, setCoins, id }) => {
+  const ref = useRef();
+  let collided = false;
+  useFrame((state) => {
+    if (ref.current) {
+      ref.current.setTranslation({
+        x: x,
+        y: Math.sin(state.clock.elapsedTime * 2.5) * 0.1 + 1.4,
+        z: z,
+      });
+    }
+  });
+
+  return (
+    <>
+      <RigidBody
+        name="coin"
+        ref={ref}
+        type="fixed"
+        colliders={"trimesh"}
+        onCollisionEnter={({ other }) => {
+          if (
+            other.rigidBodyObject &&
+            !collided && // used to render once
+            other.rigidBodyObject.name === "player"
+          ) {
+            collided = true;
+            setTimeout(() => {
+              setCoins((coins) => coins.filter((coin) => coin.props.id !== id));
+            }, 20); // creates a bumping effect
+          }
+        }}
+      >
+        <mesh rotation={[Math.PI / 2, 0, 0]} position={[x, 0, z]}>
+          <pointLight intensity={1} distance={5} decay={1.5} color={"gold"} />
+          <cylinderGeometry args={[0.2, 0.2, 0.1]} />
+          <meshStandardMaterial
+            color={"gold"}
+            roughness={0.4}
+            metalness={0.1}
+          />
+        </mesh>
+      </RigidBody>
+    </>
+  );
+};
+export default Coin;
