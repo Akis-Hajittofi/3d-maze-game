71bcf41a423ed79a8abadc74be0eeb893e01f3fb
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2024-01-02 22:57:32 +0000

    Fixed issue where if room1.x is greater than room2.x the positioning and length was wrong

--- src/App.js
+++ src/App.js
@@ -51,23 +51,53 @@ function Passage({
 }
 
 // A function that renders the passage based on the position from room1 to room2
-const GeneratePassage = ({
-  room1 = { x: 0, z: 0, size: [80, 80] },
-  room2 = { x: 200, z: 0, size: [80, 80] },
-}) => {
+const GeneratePassage = ({ room1, room2 }) => {
+  // const room1 = { x: 0, z: 0, size: [80, 80] };
+  // const room2 = { x: 200, z: 0, size: [30, 30] };
   const depth = 3;
-  const length =
-    room2.x - room1.x - room2.size[0] / 2 - room1.size[0] / 2 - depth;
+  let roomDifference;
+  let startingPosition;
+  let roomSize;
+
+  if (room1.x < room2.x) {
+    roomDifference = room2.x - room1.x;
+    startingPosition = room1.x;
+    roomSize = room1.size[0];
+  } else if (room1.x > room2.x) {
+    roomDifference = room1.x - room2.x;
+    startingPosition = room2.x;
+    roomSize = room2.size[0];
+  }
+
+  const length = roomDifference - room2.size[0] / 2 - room1.size[0] / 2 - depth;
 
   return (
     <Passage
-      position={[room1.x, 0, room1.z - 5]}
+      position={[startingPosition, 0, room1.z - 5]}
       l={length}
-      offset={(length + room1.size[0] + depth) / 2}
+      offset={(length + roomSize + depth) / 2}
       depth={depth}
     />
   );
 };
+
+// const GeneratePassage = ({ room1, room2 }) => {
+//   // const room1 = { x: 200, z: 0, size: [80, 80] };
+//   // const room2 = { x: 0, z: 0, size: [30, 30] };
+
+//   const depth = 3;
+//   const length =
+//     room1.x - room2.x - room2.size[0] / 2 - room1.size[0] / 2 - depth;
+
+//   return (
+//     <Passage
+//       position={[room2.x, 0, room1.z - 5]}
+//       l={length}
+//       offset={(length + room2.size[0] + depth) / 2}
+//       depth={depth}
+//     />
+//   );
+// };
 
 function WallWithDoor({ position, rotation, color = "black", l = 30 }) {
   const depth = 3;
@@ -170,8 +200,8 @@ function Coins() {
 }
 
 function App() {
-  const room1 = { x: 0, z: 0, size: [80, 80] };
-  const room2 = { x: 200, z: 0, size: [30, 30] };
+  const room1 = { x: 200, z: 0, size: [80, 80] };
+  const room2 = { x: 0, z: 0, size: [30, 30] };
   const room3 = { x: -300, z: 0, size: [50, 50] };
   const room4 = { x: -150, z: 0, size: [70, 70] };
 
